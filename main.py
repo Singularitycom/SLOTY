3cbaf08e079de341eff35de192be77e4998d1612
Singularitycom <dcales77@gmail.com>
2025-09-09 03:32:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+from flask import Flask
+from threading import Thread
 import discord
 from discord.ext import commands
 import os
 
+# --- Keep Alive Webserver ---
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "I'm alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+# --- Discord Bot Setup ---
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -14,4 +31,6 @@ async def on_ready():
 async def hello(ctx):
     await ctx.send(f"Hello {ctx.author.mention}! I'm alive 🚀")
 
+# --- Start bot ---
+keep_alive()
 bot.run(os.environ["TOKEN"])
